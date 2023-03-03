1dae9fd82064a4cc84a69c041371abccc68378cc
José Ithalo <jose_ithalo@hotmail.com>
2023-03-03 13:56:12 -0300

    Desenvolvendo o card da rota

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,15 +1,18 @@
 import './Home.css';
 
 import plusSymbol from "../assets/plusSymbol.svg";
-import planeIcon from "../assets/planeIcon.svg";
-import discover from "../assets/discover.svg";
-import travellersIcon from "../assets/travellersIcon.svg";
-import likes from "../assets/likes.svg";
+
+import photo from "../assets/photoSushi.svg";
 
 import taxiIcon from "../assets/taxi.svg";
 import subwayIcon from "../assets/subway.svg";
 import uberIcon from "../assets/uber.svg";
 import uberIcon2 from "../assets/uber2.svg";
+
+import planeIcon from "../assets/planeIcon.svg";
+import discover from "../assets/discover.svg";
+import travellersIcon from "../assets/travellersIcon.svg";
+import likes from "../assets/likes.svg";
 
 function Home() {
   return (
@@ -26,7 +29,23 @@ function Home() {
         </section>
 
         <section className="tird-section card">
-          <div></div>
+          <div className="body-reminders">
+            <div className="driver-info">
+              <img src={photo} alt="The driver" />
+              <p>
+                Ryan from Corey Car Services is confirmed for pickup at
+                <span style={{ color: '#8BC34A' }}> 15.40</span>.
+              </p>
+            </div>
+
+            <div className="route">
+              <div className="pickup-area">
+                <div className="circle">
+                  <div className="circle-pickup"></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
 
         <section className="fourth-section card"></section>

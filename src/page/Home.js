bf45899c142a0ef614605baf04c865a0472895ec
José Ithalo <jose_ithalo@hotmail.com>
2023-03-04 16:02:12 -0300

    Finalização do boardingpass

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,6 @@
 import './Home.css';
 
+import travelPlane from "../assets/travel.svg";
 import plusSymbol from "../assets/plusSymbol.svg";
 
 import photo from "../assets/photoSushi.svg";
@@ -19,11 +20,55 @@ function Home() {
   return (
     <div className="container-home">
       <section className="first-section card">
-        <h1>Let’s get <br /> you there…</h1>
+        <h1 className="first-title">Let’s get <br /> you there…</h1>
         <div className="boardingPass">
           <div className="top-info">
             <p className="top-info-first">BOARDING NOW</p>
             <p className="top-info-second">GATE CLOSES AT 10.00</p>
+          </div>
+          <div className="boardingPass-body">
+            <div className="boardingPass-second">
+
+              <div className="boardingPass-city">
+                <h1>LHR</h1>
+                <span>London, England</span>
+              </div>
+
+              <div className="boardingPass-img">
+                <div className="boardingPass-img-line"></div>
+                <img src={travelPlane} alt="Icon of plane" />
+                <div className="boardingPass-img-line"></div>
+              </div>
+
+              <div className="boardingPass-city">
+                <h1>JFK</h1>
+                <span>New York City, USA</span>
+              </div>
+            </div>
+            <div className="boardingPass-tird">
+              <div className="boardingPass-tird-info">
+                <div className="boardingPass-tird-titles">
+                  <h4>FLIGHT</h4>
+                  <h3>VA2501</h3>
+                </div>
+                <div className="boardingPass-tird-titles">
+                  <h4>SEAT</h4>
+                  <h3>35a</h3>
+                </div>
+                <div className="boardingPass-tird-titles">
+                  <h4>GATE</h4>
+                  <h3>34B</h3>
+                </div>
+              </div>
+
+              <div className="boardingPass-tird-info">
+                <div className="boardingPass-tird-titles">
+                  <h4>DEPARTS</h4>
+                  <h3>10.25</h3>
+                </div>
+                <button>VIEW BOARDING PASS</button>
+              </div>
+            </div>
           </div>
         </div>
       </section>

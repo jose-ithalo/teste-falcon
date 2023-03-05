4064868232d7a0fa94f223601c49e9ea1a10def2
José Ithalo <jose_ithalo@hotmail.com>
2023-03-04 22:56:00 -0300

    Alteração do caminho para home.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
-import Home from './page/Home';
+import Home from './page/home.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
